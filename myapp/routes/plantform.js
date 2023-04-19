dcc0cff05ed0ad8f3bdd3c0ed41a4c9e79f5b49f
孔令召 <450652415@qq.com>
2023-04-19 23:26:23 +0800

    代码整理

--- myapp/routes/plantform.js
+++ myapp/routes/plantform.js
@@ -8,29 +8,18 @@ router.get('/', (req, res, next) => {
     // 检测是否为 Android 设备
     if (userAgent.toLowerCase().includes('android')) {
         // 如果是 Android 设备，重定向到 Android 界面
-        res.redirect('http://localhost:3000/static/images/android.txt');
+        res.redirect('/static/images/android.txt');
         return;
     }
 
     // 检测是否为 iOS 设备
     if (userAgent.toLowerCase().includes('iphone') || userAgent.toLowerCase().includes('ipad')) {
         // 如果是 iOS 设备，重定向到 iOS 界面
-        res.redirect('http://localhost:3000/static/images/ios.txt');
+        res.redirect('/static/images/ios.txt');
         return;
     }
-
     // 其他情况，继续请求处理
     next();
-});
-
-// Android 界面路由
-router.get('/android', (req, res) => {
-    res.send('欢迎访问 Android 界面！');
-});
-
-// iOS 界面路由
-router.get('/ios', (req, res) => {
-    res.send('欢迎访问 iOS 界面！');
 });
 
 // 默认界面路由
